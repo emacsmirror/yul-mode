56cba05549873fcf1b66e304969011dc1a1ad228
Ta Quang Trung <taquangtrungvn@gmail.com>
2022-09-27 11:38:05 +0800

    support indent line function for Yul syntax

--- yul-mode.el
+++ yul-mode.el
@@ -3,8 +3,8 @@
 ;; Copyright © 2022, by Ta Quang Trung
 
 ;; Author: Ta Quang Trung
-;; Version: 0.0.1
-;; Created: 30 July 2022
+;; Version: 0.0.2
+;; Created: 27 September 2022
 ;; Keywords: languages
 ;; Homepage: https://github.com/taquangtrung/emacs-yul-mode
 
@@ -98,6 +98,48 @@ Highlight the 1st result."
    '(yul--match-functions (1 font-lock-function-name-face)))
   "Font lock keywords of `yul-mode'.")
 
+;;;;;;;;;;;;;;;;;;
+;;; Indentation
+
+(defun yul-indent-line (&optional indent)
+  "Indent the current line according to the Yul syntax, or supply INDENT."
+  (interactive "P")
+  (let ((pos (- (point-max) (point)))
+        (indent (or indent (yul--calculate-indentation)))
+        (shift-amount nil)
+        (beg (progn (beginning-of-line) (point))))
+    (skip-chars-forward " \t")
+    (if (null indent)
+        (goto-char (- (point-max) pos))
+      (setq shift-amount (- indent (current-column)))
+      (unless (zerop shift-amount)
+        (delete-region beg (point))
+        (indent-to indent))
+      (when (> (- (point-max) pos) (point))
+        (goto-char (- (point-max) pos))))))
+
+(defun yul--calculate-indentation ()
+  "Calculate the indentation of the current line."
+  (let (indent)
+    (save-excursion
+      (back-to-indentation)
+      (let* ((ppss (syntax-ppss))
+             (depth (car ppss))
+             (paren-start-pos (cadr ppss))
+             (base (* tab-width depth)))
+        (unless (= depth 0)
+          (setq indent base)
+          (cond ((looking-at "\s*[})]")
+                 ;; closing a block or a parentheses pair
+                 (setq indent (- base tab-width)))
+                ((looking-at "\s*:=")
+                 ;; indent for multiple-line assignment
+                 (setq indent (+ base (* 2 tab-width))))
+                ((looking-back "\s*:=\s*\n\s*")
+                 ;; indent for multiple-line assignment
+                 (setq indent (+ base (* 2 tab-width))))))))
+    indent))
+
 ;;;;;;;;;;;;;;;;;;;;;
 ;;; Imenu settings
 
@@ -128,27 +170,22 @@ Highlight the 1st result."
   (setq font-lock-defaults '(yul-font-lock-keywords))
 
   ;; Indentation
-  (set (make-local-variable 'indent-tabs-mode) nil)
-  (set (make-local-variable 'indent-line-function) 'indent-relative)
+  (setq-local indent-tabs-mode nil)
+  (setq-local indent-line-function #'yul-indent-line)
 
   ;; Set comment command
-  (set (make-local-variable 'comment-start) "//")
-  (set (make-local-variable 'comment-end) "")
-  (set (make-local-variable 'comment-multi-line) nil)
-  (set (make-local-variable 'comment-use-syntax) t)
+  (setq-local comment-start "//")
+  (setq-local comment-end "")
+  (setq-local comment-multi-line nil)
+  (setq-local comment-use-syntax t)
 
   ;; Configure imenu
-  (set (make-local-variable 'imenu-create-index-function)
-       'yul--imenu-create-index))
+  (setq-local imenu-create-index-function #'yul--imenu-create-index))
 
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.yul\\'" . yul-mode))
 
 ;; Finally export the `yul-mode'
 (provide 'yul-mode)
-
-;; Local Variables:
-;; coding: utf-8
-;; End:
 
 ;;; yul-mode.el ends here
